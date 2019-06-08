405c7f85b36ac810428d71a3bb2bcdd67ccd683e
Shota Zenaishvili <shotazenaishvili@gmail.com>
2019-06-08 13:08:55 +0400

    git last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularTask';
+  
+
+  tagArray1: number[] = [];
+
+  text: any = '';
+  editingTags: any = '';
+
+  addTagFunction(){
+    console.log(this.text);
+    if(this.text == ''){
+      return;
+    }
+    this.fillTagArray1ArrayFromString(this.text)
+    this.text = '';
+  }
+  deleteText(index){
+    console.log(index)
+    for(let i=0; i<this.tagArray1.length; i++){
+      if(this.tagArray1[i] == this.tagArray1[index]){
+        this.tagArray1.splice(index, 1);
+        console.log(this.tagArray1[index]);
+      }
+    }
+  }
+
+  editTag(){
+    console.log(this.tagArray1);
+    this.editingTags = this.tagArray1;
+  }
+  
+  updateTag(){
+    this.fillTagArray1ArrayFromString(this.editingTags);
+    this.editingTags = '';
+  }
+
+  fillTagArray1ArrayFromString(text){
+    console.log(this.editingTags);
+    this.tagArray1 = [];
+    var tempTagarray = text.toString().split(' ').join(',').split(';').join(',').split(',');
+    
+    for(let i=0; i<tempTagarray.length; i++){
+      if(isNaN(tempTagarray[i]) == false){
+        console.log(tempTagarray[i]);
+        this.tagArray1.push(tempTagarray[i]);
+      }
+    }
+  }
+
+
+
 }
