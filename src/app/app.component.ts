8e374ebb58aef7bae8df7f1d137a9df08a2d6fcd
Shota Zenaishvili <shotazenaishvili@gmail.com>
2019-06-08 13:55:19 +0400

    update-add local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit}  from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
 
-  tagArray1: number[] = [];
+  tagArray: number[] = [];
 
   text: any = '';
   editingTags: any = '';
@@ -23,17 +23,18 @@ export class AppComponent {
   }
   deleteText(index){
     console.log(index)
-    for(let i=0; i<this.tagArray1.length; i++){
-      if(this.tagArray1[i] == this.tagArray1[index]){
-        this.tagArray1.splice(index, 1);
-        console.log(this.tagArray1[index]);
+    for(let i=0; i<this.tagArray.length; i++){
+      if(this.tagArray[i] == this.tagArray[index]){
+        this.tagArray.splice(index, 1);
+        console.log(this.tagArray[index]);
       }
     }
+    localStorage.setItem("tags", JSON.stringify(this.tagArray));
   }
 
   editTag(){
-    console.log(this.tagArray1);
-    this.editingTags = this.tagArray1;
+    console.log(this.tagArray);
+    this.editingTags = this.tagArray;
   }
   
   updateTag(){
@@ -43,15 +44,21 @@ export class AppComponent {
 
   fillTagArray1ArrayFromString(text){
     console.log(this.editingTags);
-    this.tagArray1 = [];
+    this.tagArray = [];
     var tempTagarray = text.toString().split(' ').join(',').split(';').join(',').split(',');
     
     for(let i=0; i<tempTagarray.length; i++){
       if(isNaN(tempTagarray[i]) == false){
         console.log(tempTagarray[i]);
-        this.tagArray1.push(tempTagarray[i]);
+        this.tagArray.push(tempTagarray[i]);
       }
     }
+    localStorage.setItem("tags", JSON.stringify(this.tagArray));
+  }
+
+  ngOnInit() {
+    console.log(localStorage.getItem('tags'));
+    this.tagArray = JSON.parse(localStorage.getItem("tags"));
   }
 
 
